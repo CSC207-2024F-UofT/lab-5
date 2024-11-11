114215af2bb5e0ce87cb99efdfc734cc4f9c229a
Adya Gopaul <adya.gopaul@mail.utoronto.ca>
2024-11-11 16:52:04 -0500

    partial implementation of recipe search view

---
+++ src/main/java/interface_adapter/recipe_search/RecipeSearchViewModel.java
@@ -0,0 +1,9 @@
+package interface_adapter.recipe_search;
+
+/**
+ * The ViewModel for the RecipeSearch View.
+ */
+
+public class RecipeSearchViewModel {
+    public static final String
+}
---
+++ src/main/java/use_case/search_recipe/SeachRecipe.java
@@ -0,0 +1,5 @@
+package use_case.search_recipe;
+
+public class SeachRecipe {
+
+}
