04de60fd5edd0648044b64d15080a82a5cb8538d
cindyzhang <cxy.zhang16@gmail.com>
2024-11-12 20:34:38 -0500

    added methods to User and Recipe, modified BookmarkView, and added test display in Main for bookmarks page

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,17 @@
 package app;
 
+import entity.Recipe;
 import entity.User;
 import view.BookmarkView;
 import view.LoginSignupPage;
 
+import java.util.List;
+
 public class Main {
-    // mock user object to test bookmarks page
+    // mock objects to test bookmarks page
     private static User user;
+    private static Recipe recipe1;
+    private static Recipe recipe2;
 
     public static void main(String[] args) {
         // Launch the login/signup page as the first view
@@ -14,6 +19,10 @@ public class Main {
 
         // Temporarily launch the bookmarks page upon running the program
         user = new User("Test_username", "Test_password");
+        recipe1 = new Recipe("name1", "url1", List.of("a", "b", "c"));
+        recipe2 = new Recipe("name2", "url2", List.of("a", "b", "c"));
+        user.addBookmark(recipe1);
+        user.addBookmark(recipe2);
         new BookmarkView(user);
     }
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,13 +1,17 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
     private String password;
-
+    private List<Recipe> bookmarks;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.bookmarks = new ArrayList<>();
     }
 
     // Getters and Setters
@@ -25,5 +29,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Recipe> getBookmarks() {
+        return bookmarks;
+    }
+
+    /*
+    Add a recipe to the list of bookmarks.
+     */
+    public void addBookmark(Recipe recipe) {
+        this.bookmarks.add(recipe);
     }
 }
