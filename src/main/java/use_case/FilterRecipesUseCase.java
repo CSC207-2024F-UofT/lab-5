65cfb269e67c2cd502ca0c45707d6e201a35ac10
kayzoo8 <kk.zhou@mail.utoronto.ca>
2024-11-17 19:24:10 -0500

    added a filter use case class and filterByDiet method for now

--- src/main/java/use_case/FilterRecipesUseCase.java
+++ src/main/java/use_case/FilterRecipesUseCase.java
@@ -1,8 +1,38 @@
 package use_case;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import data_access.RecipeDAO;
+import entity.DietaryPreference;
+import entity.Recipe;
+
 /**
  * Use case layer for filtering recipes.
  */
 public class FilterRecipesUseCase {
     // to be implemented
+    // database from api?
+    private final RecipeDAO recipeDao;
+
+    public FilterRecipesUseCase(RecipeDAO recipeDao) {
+        this.recipeDao = recipeDao;
+    }
+
+    /**
+     * Get the recipes according to the dietary preference.
+     * @param diet user's diet choice
+     * @return a list of recipes that satisfy diet choice
+     */
+    public List<Recipe> filterByDiet(String diet) {
+        final List<Recipe> recipes = recipeDao.getAllRecipes();
+        final List<Recipe> filteredRecipes = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            if (recipe.getDietaryType().equals(diet)) {
+                filteredRecipes.add(recipe);
+            }
+        }
+        return filteredRecipes;
+    }
+
 }
