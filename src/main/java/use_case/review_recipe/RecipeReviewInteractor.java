8e95810107923599ee32103c16c70c5a8c64615d
Adya Gopaul <adya.gopaul@mail.utoronto.ca>
2024-11-22 14:35:24 -0500

    creation of classes and interfaces for recipe review use case

--- src/main/java/use_case/review_recipe/RecipeReviewInteractor.java
+++ src/main/java/use_case/review_recipe/RecipeReviewInteractor.java
@@ -1,5 +1,15 @@
 package use_case.review_recipe;
 
+/**
+ * The Recipe Review Interactor.
+ */
 public class RecipeReviewInteractor {
+    private RecipeReviewOutputBoundary userPresenter;
+    public void execute() {
 
+    }
+
+    public void switchToRecipeHistoryViewView() {
+        userPresenter.switchToRecipeHistoryView();
+    }
 }
