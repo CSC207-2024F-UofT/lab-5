a7459b08c139fd5440df1be6284cbdff5cfc9586
Adya Gopaul <adya.gopaul@mail.utoronto.ca>
2024-11-23 14:35:21 -0500

    reviewrecipe partial code

--- src/main/java/use_case/review_recipe/ReviewRecipe.java
+++ src/main/java/use_case/review_recipe/ReviewRecipe.java
@@ -1,5 +1,17 @@
 package use_case.review_recipe;
 
 public class ReviewRecipe {
+    /*
+    click review recipe button on
+    need another view
+     */
 
+    /*
+    public reviewrecipeview() {
+        final JLabel name = recipe name
+        final jtextpanel with review
+        final jtextpanel with rating
+        jbutton for submit review
+        takes you to recipe details view
+     */
 }
