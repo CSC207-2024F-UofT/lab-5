b333c54082756fbe36638385e09e97b4a41ebf55
cindyzhang <cxy.zhang16@gmail.com>
2024-11-24 20:19:58 -0500

    Split recipeListView search function into two buttons; made the LoginSignupPage close when login is successful

--- src/main/java/view/RecipeListView.java
+++ src/main/java/view/RecipeListView.java
@@ -5,34 +5,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.*;
 
-import data_access.RecipeDAO;
-import data_access.SpoonacularRecipeDAO;
 import entity.Recipe;
 import entity.User;
-import interface_adapter.RecipeController;
 import interface_adapter.RecipeListController;
 import use_case.SearchRecipeListUseCase;
-import use_case.SearchRecipeUseCase;
 
 public abstract class RecipeListView extends JFrame implements ActionListener {
     // attributes for default view
+    private static User user;
     private final JList<Recipe> recipeList;
     private final DefaultListModel<Recipe> listModel;
     private final RecipeListController controller;
 
     // TODO attributes for other functionalities...
-    private JTextField searchField;
-    private JButton searchButton;
+    private JTextField ingredientSearchField;
+    private JButton ingredientSearchButton;
+    private JTextField recipeSearchField;
+    private JButton recipeSearchButton;
 
     /*
     Generates the default view of a list of recipes associated with a User.
      */
     public RecipeListView(User user) {
+        this.user = user;
         this.recipeList = new JList<>();
         this.listModel = new DefaultListModel<>();
         this.controller = new RecipeListController(new SearchRecipeListUseCase(getRecipeList(user)));
@@ -50,7 +51,7 @@ public abstract class RecipeListView extends JFrame implements ActionListener {
         // Wrap the JList in a JScrollPane
         final JScrollPane scrollPane = new JScrollPane(recipeList);
         scrollPane.setPreferredSize(recipeList.getPreferredScrollableViewportSize());
-        add(scrollPane);
+        add(scrollPane, BorderLayout.SOUTH);
 
         // TODO fix the layout
 //        // Main panel with vertical BoxLayout
@@ -77,17 +78,24 @@ public abstract class RecipeListView extends JFrame implements ActionListener {
             }
         });
 
-        // Display search bar
-        searchField = new JTextField(20);
-        searchButton = new JButton("Search");
+        // Display the two search bars
+        ingredientSearchField = new JTextField(20);
+        ingredientSearchButton = new JButton("Search recipes by ingredients");
+        final JPanel ingredientSearchPanel = new JPanel();
+        ingredientSearchPanel.add(new JLabel("Enter ingredients (comma-separated):"));
+        ingredientSearchPanel.add(ingredientSearchField);
+        ingredientSearchPanel.add(ingredientSearchButton);
+        ingredientSearchButton.addActionListener(this);
+        add(ingredientSearchPanel, BorderLayout.NORTH);
 
-        final JPanel panel = new JPanel();
-        panel.add(new JLabel("Enter ingredients (comma-separated):"));
-        panel.add(searchField);
-        // TODO add ActionListener for the search button
-        panel.add(searchButton);
-        searchButton.addActionListener(this);
-        add(panel, BorderLayout.NORTH);
+        recipeSearchField = new JTextField(20);
+        recipeSearchButton = new JButton("Search recipes by name");
+        final JPanel recipeSearchPanel = new JPanel();
+        recipeSearchPanel.add(new JLabel("Enter recipe name:"));
+        recipeSearchPanel.add(recipeSearchField);
+        recipeSearchPanel.add(recipeSearchButton);
+        recipeSearchButton.addActionListener(this);
+        add(recipeSearchPanel, BorderLayout.CENTER);
 
         // TODO Search function - in progress
 //        ingredientInput = new JTextField(20);
@@ -117,15 +125,16 @@ public abstract class RecipeListView extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent event) {
         // temporary display for search button
-        final String userInput = searchField.getText();
-        // Display the input in the result label
-        JOptionPane.showMessageDialog(this, userInput);
+        if (event.getSource() == ingredientSearchButton) {
+            final String userInput = ingredientSearchField.getText();
+            // TODO - replace: Display the input in the result label
+            JOptionPane.showMessageDialog(this, userInput);
 
-        // TODO complete search function...
+            // TODO complete search function...
 
-        final List<String> ingredients = Arrays.asList(userInput.split(","));
-        // ^ TODO need to convert user input to Ingredient objects!
-        // final List<Recipe> recipes = controller.getRecipes(ingredients);
+            final List<String> ingredients = Arrays.asList(userInput.split(","));
+            // ^ TODO need to convert user input to Ingredient objects!
+            // final List<Recipe> recipes = controller.getRecipes(ingredients);
 //
 //        final DefaultListModel<Recipe> listModel = new DefaultListModel<>();
 //        for (Recipe recipe : recipes) {
@@ -149,6 +158,31 @@ public abstract class RecipeListView extends JFrame implements ActionListener {
 //                }
 //            }
 //        });
+        }
+        if (event.getSource() == recipeSearchButton) {
+            // TODO convert this into a use case
+            final String userInput = recipeSearchField.getText();
+            final List<Recipe> recipes = getRecipeList(user);
+            final List<Recipe> results = new ArrayList<>();
+            final DefaultListModel<Recipe> resultListModel = new DefaultListModel<>();
+            final JList<Recipe> resultList = new JList<>(listModel);
+            // TODO search by name function...
+            for (Recipe recipe : recipes) {
+                if (recipe.getName().contains(userInput)) {
+                    results.add(recipe);
+                }
+            }
+            // temporary display
+            JOptionPane.showMessageDialog(this, results);
+//            for (Recipe result : results) {
+//                resultListModel.addElement(result);
+//            }
+//            resultList.setModel(resultListModel);
+//            final JFrame resultFrame = new JFrame();
+//            resultFrame.setTitle("Search results");
+//            resultFrame.add(resultList);
+//            setVisible(true);
+        }
     }
 
     protected abstract List<Recipe> getRecipeList(User user1);
