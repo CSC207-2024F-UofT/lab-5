b808fe9591e5de36267b60751705d0aeb005de9d
cindyzhang <cxy.zhang16@gmail.com>
2024-11-25 13:23:33 -0500

    fixed bookmark controller

--- src/main/java/interface_adapter/BookmarkController.java
+++ src/main/java/interface_adapter/BookmarkController.java
@@ -5,11 +5,13 @@ In progress; commented out everything for now so it doesn't crash the program.
 package interface_adapter;
 
 import use_case.SearchRecipeListByIngredientUseCase;
+import use_case.SearchRecipeListByNameUseCase;
 
 public class BookmarkController extends RecipeListController {
 
-    public BookmarkController(SearchRecipeListByIngredientUseCase searchRecipeListByIngredientUseCase) {
-        super(searchRecipeListByIngredientUseCase);
+    public BookmarkController(SearchRecipeListByIngredientUseCase searchRecipeListByIngredientUseCase,
+                              SearchRecipeListByNameUseCase searchRecipeListByNameUseCase) {
+        super(searchRecipeListByIngredientUseCase, searchRecipeListByNameUseCase);
     }
 
 //    public addBookmark(Recipe recipe) {
