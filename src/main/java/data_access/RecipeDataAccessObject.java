a51ec19e0ff36e546719ae5f3eb296075747f97a
noorafrancis <noorafrancis@mail.tau.ac.il>
2024-12-01 15:21:23 -0500

    fixed a checkstyle error

--- src/main/java/data_access/RecipeDataAccessObject.java
+++ src/main/java/data_access/RecipeDataAccessObject.java
@@ -3,10 +3,8 @@ package data_access;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,84 +32,37 @@ public class RecipeDataAccessObject implements RecipeSearchDataAccessInterface {
         this.customTask = null;
     }
 
+    private String buildRangeQuery(String key, String min, String max) {
+        String val = "";
+        if (!"".equals(min) && !"".equals(max)) {
+            val = String.format("&%s=%s-%s", key, min, max);
+        }
+        else if (!"".equals(min)) {
+            val = String.format("&%s=%s%%2B", key, min);
+        }
+        else if (!"".equals(max)) {
+            val = String.format("&%s=%s", key, max);
+        }
+        return val;
+    }
+
     @Override
     public List<Recipe> searchRecipe(String q, String cal_min, String cal_max, String carb_min, String carb_max, String protein_min, String protein_max, String fat_min, String fat_max) {
-        final OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+        // Build query parameters
         String newq = "";
         if (!"".equals(q)) {
             newq = "&q=" + q;
         }
-        String calories1 = "";
-        if (!"".equals(cal_min)) {
-            if (!"".equals(cal_max)) {
-                calories1 = "&calories=" + cal_min + "-" + cal_max;
-            }
-        }
-        if (!"".equals(cal_min)) {
-            if ("".equals(cal_max)) {
-                calories1 = "&calories=" + cal_min + "%2B";
-            }
-        }
-        if ("".equals(cal_min)) {
-            if (!"".equals(cal_max)) {
-                calories1 = "&calories=" + cal_max;
-            }
-        }
-
-        String carbs = "";
-        if (!"".equals(carb_min)) {
-            if (!"".equals(carb_max)) {
-                carbs = "&nutrients%5BCHOCDF%5D=" + carb_min + "-" + carb_max;
-            }
-        }
-        if (!"".equals(carb_min)) {
-            if ("".equals(carb_max)) {
-                carbs = "&nutrients%5BCHOCDF%5D=" + carb_min + "%2B";
-            }
-        }
-        if ("".equals(carb_min)) {
-            if (!"".equals(carb_max)) {
-                carbs = "&nutrients%5BCHOCDF%5D=" + carb_max;
-            }
-        }
-
-        String protein = "";
-        if (!"".equals(protein_min)) {
-            if (!"".equals(protein_max)) {
-                protein = "&nutrients%5BPROCNT%5D=" + protein_min + "-" + protein_max;
-            }
-        }
-        if (!"".equals(protein_min)) {
-            if ("".equals(protein_max)) {
-                protein = "&nutrients%5BPROCNT%5D=" + protein_min + "%2B";
-            }
-        }
-        if ("".equals(protein_min)) {
-            if (!"".equals(protein_max)) {
-                protein = "&nutrients%5BPROCNT%5D=" + protein_max;
-            }
-        }
-
-        String fat = "";
-        if (!"".equals(fat_min)) {
-            if (!"".equals(fat_max)) {
-                fat = "&nutrients%5BPROCNT%5D=" + fat_min + "-" + fat_max;
-            }
-        }
-        if (!"".equals(fat_min)) {
-            if ("".equals(fat_max)) {
-                fat = "&nutrients%5BPROCNT%5D=" + fat_min + "%2B";
-            }
-        }
-        if ("".equals(fat_min)) {
-            if (!"".equals(fat_max)) {
-                fat = "&nutrients%5BPROCNT%5D=" + fat_max;
-            }
-        }
+        final String calories = buildRangeQuery("calories", cal_min, cal_max);
+        final String carbs = buildRangeQuery("nutrients%5BCHOCDF%5D", carb_min, carb_max);
+        final String protein = buildRangeQuery("nutrients%5BPROCNT%5D", protein_min, protein_max);
+        final String fat = buildRangeQuery("nutrients%5BFAT%5D", fat_min, fat_max);
 
         final Request request = new Request.Builder()
-                .url(String.format("%s?type=public%s&app_id=%s&app_key=%s%s%s%s%s", API_URL, newq, APP_ID, APP_KEY, calories1, carbs, fat, protein))
+                .url(String.format("%s?type=public%s&app_id=%s&app_key=%s%s%s%s%s",
+                        API_URL, newq, APP_ID, APP_KEY, calories, carbs, protein, fat))
                 .build();
 
         try {
@@ -134,9 +85,9 @@ public class RecipeDataAccessObject implements RecipeSearchDataAccessInterface {
                 // Extract recipe details
                 final String name = recipeObj.getString("label");
                 final int servings = recipeObj.optInt("yield", 1);
-                final int calories = (int) Math.round(recipeObj.getDouble("calories") / servings);
+                final int caloriesValue = (int) Math.round(recipeObj.getDouble("calories") / servings);
                 final String url = recipeObj.getString("url");
-                final String image = recipeObj.getJSONObject("images").getJSONObject("THUMBNAIL").getString("url");
+                // final String image = recipeObj.getJSONObject("images").getJSONObject("THUMBNAIL").getString("url");
 
                 // Extract nutrients
                 final Map<String, Integer> nutrients = new HashMap<>();
@@ -146,22 +97,22 @@ public class RecipeDataAccessObject implements RecipeSearchDataAccessInterface {
                     nutrients.put(nutrient.getString("label"), (int) nutrient.getDouble("quantity"));
                 }
 
-                // Extract tags
-                final Set<String> tags = new HashSet<>();
-                final JSONArray healthLabels = recipeObj.getJSONArray("healthLabels");
-                for (int j = 0; j < healthLabels.length(); j++) {
-                    tags.add(healthLabels.getString(j));
-                }
+                //                // Extract tags
+                //                final Set<String> tags = new HashSet<>();
+                //                final JSONArray healthLabels = recipeObj.getJSONArray("healthLabels");
+                //                for (int j = 0; j < healthLabels.length(); j++) {
+                //                    tags.add(healthLabels.getString(j));
+                //                }
 
                 // Build the Recipe object
                 final Recipe recipe = Recipe.builder()
                         .name(name)
                         .servings(servings)
-                        .calories(calories)
+                        .calories(caloriesValue)
                         .nutrients(nutrients)
-                        .tags(tags)
+                        // .tags(tags)
                         .url(url)
-                        .image(image)
+                        // .image(image)
                         .build();
 
                 recipes.add(recipe);
